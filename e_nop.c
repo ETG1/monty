b5955ef256c2a182b85c9ab6e9dc45f18d821868
ETG1 <elijahkazungu22@gmail.com>
2023-08-24 17:53:54 -0700

    Updated

---
+++ e_nop.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+  *e_nop- nothing
+  *@head: stack head
+  *@counter: line_number
+  *
+  *Return: no return
+ */
+
+void e_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
